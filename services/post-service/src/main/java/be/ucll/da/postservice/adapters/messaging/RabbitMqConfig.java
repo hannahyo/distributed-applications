4f262577538e1e8607e21551f59160eee1c62d9d
Hannah Young <hannah.young@student.ucll.be>
2025-01-08 12:39:11 +0100

    rabbitmq config for notification

--- services/post-service/src/main/java/be/ucll/da/postservice/adapters/messaging/RabbitMqConfig.java
+++ services/post-service/src/main/java/be/ucll/da/postservice/adapters/messaging/RabbitMqConfig.java
@@ -1,6 +1,6 @@
 package be.ucll.da.postservice.adapters.messaging;
 
-import org.springframework.amqp.core.Queue;
+import org.springframework.amqp.core.*;
 import org.springframework.amqp.rabbit.connection.CachingConnectionFactory;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
@@ -11,8 +11,26 @@ import org.springframework.context.annotation.Configuration;
 public class RabbitMqConfig {
 
     @Bean
-    public Queue createUserTaggedQueue() {
-        return new Queue("q.user-tagged");
+    public Declarables createUserTaggedSchema(){
+        return new Declarables(
+                new FanoutExchange("x.user-tagged"),
+                new Queue("q.post-user-service" ),
+                QueueBuilder.durable("q.post-notification-service")
+                        .withArgument("x-dead-letter-exchange","x.notification-failure")
+                        .withArgument("x-dead-letter-routing-key","fall-back")
+                        .build(),
+                new Binding("q.post-user-service", Binding.DestinationType.QUEUE, "x.user-tagged", "post-user-service", null),
+                new Binding("q.post-notification-service", Binding.DestinationType.QUEUE, "x.user-tagged", "post-notification-service", null));
+
+    }
+
+    @Bean
+    public Declarables createDeadLetterSchema(){
+        return new Declarables(
+                new DirectExchange("x.notification-failure"),
+                new Queue("q.fall-back-notification"),
+                new Binding("q.fall-back-notification", Binding.DestinationType.QUEUE,"x.notification-failure", "fall-back", null)
+        );
     }
 
     @Bean
--- services/user-service/src/main/java/be/ucll/da/userservice/adapters/messaging/MessageListener.java
+++ services/user-service/src/main/java/be/ucll/da/userservice/adapters/messaging/MessageListener.java
@@ -4,6 +4,7 @@ import be.ucll.da.userservice.domain.UserService;
 import jakarta.transaction.Transactional;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
