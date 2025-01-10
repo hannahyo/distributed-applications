a36b742a070baf5d1d23620d516e9590046c9efd
Hannah Young <hannah.young@student.ucll.be>
2025-01-10 14:58:51 +0100

    initial api-gateway

---
+++ services/api-gateway/src/test/java/be/ucll/da/apigateway/ApigatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package be.ucll.da.apigateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApigatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
